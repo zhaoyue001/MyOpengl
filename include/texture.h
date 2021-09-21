5000428fbc0faa384faa85428939b83208862b7d
husky <zhaoyue350301@163.com>
2021-09-21 15:54:35 +0800

    cmaera classs

--- include/texture.h
+++ include/texture.h
@@ -2,7 +2,7 @@
 #define TEXTURE_H
 #include <glad/glad.h>
 #include <iostream>
-//#include "stb_image.h"
+
 class Texture {
 private:
   unsigned char *picture;
--- src/test.cpp
+++ src/test.cpp
@@ -6,6 +6,7 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <iostream>
 #include <texture.h>
+
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 
